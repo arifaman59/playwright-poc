01697a9b3b68b137d83fa5456ed27db7c1cb0c51
Arif Aman <arifaman@Arifs-MacBook-Pro.local>
2025-11-09 15:42:45 +1100

    test

--- fetch-screens.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git fetch origin visuals
-git checkout origin/visuals -- tests/visual-tests.spec.ts-snapshots/
--- git-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-COMMIT_MSG=$1
-git add .
-git commit -m "$COMMIT_MSG"
-git push origin main
