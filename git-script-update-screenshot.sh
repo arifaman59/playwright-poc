e7f160a8a9d00521c1421454ce9bf9f12b2fbf93
Arif Aman <arifaman@Arifs-MacBook-Pro.local>
2025-11-09 22:19:11 +1100

    added script to upload screenshots

--- git-script-update-screenshot.sh
+++ git-script-update-screenshot.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
 COMMIT_MSG=$1
-git add .
+git add tests/visual-tests.spec.ts-snapshots/
 git commit -m "$COMMIT_MSG"
-git push origin main
+git push origin $(git subtree split --prefix=tests/visual-tests.spec.ts-snapshots/ main):visuals
+
+if [ $? -eq 0 ]; then
+  echo "✅ Successfully updated the screens in git"
+else
+  echo "❌ Error updating screenshots in git"
+fi
